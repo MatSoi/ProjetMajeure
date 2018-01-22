e83b0f0a955e1e010e6ea66ecbd145deb98a6d1e
Matthieu Soignon <33734431+MatSoi@users.noreply.github.com>
2018-01-22 11:45:18 +0100

    Update shader + scene handler scripts

--- Unity_Joueur/Assets/script/Scene_Handler/Scene.cs
+++ Unity_Joueur/Assets/script/Scene_Handler/Scene.cs
@@ -7,11 +7,10 @@ public class Scene : MonoBehaviour
 {
 
     public GameObject udpReceiver;
+	public GameObject udp2CppReceiver;
 
     [SerializeField]
-    private string path = "Assets/Inputs/calibrationOutput.txt";
-    [SerializeField]
-    private GameObject lCamera;
+	private GameObject lCamera;
     [SerializeField]
     private GameObject rCamera;
     [SerializeField]
@@ -23,27 +22,22 @@ public class Scene : MonoBehaviour
     [SerializeField]
     private GameObject player3;
 
-    private Vector3 leftCameraPos;
-    private Vector3 rightCameraPos;
-    private Vector3 markerBoardPos;
-    private List<Vector3> markerPlayerPos;
-
     private string order;
     Orders currentOrder = new Orders();
 
+	private string calibrationOrder;
+	CalibrationOrders currentCalibrationOrder = new CalibrationOrders();
+
     public Scene()
     {
-        markerPlayerPos = new List<Vector3>();
+		//AndroidJavaClass jc = new AndroidJavaClass("java.lang.Object");
+
     }
 
 
     // Use this for initialization
     void Start()
     {
-        readCalibrationData(path);
-        //setScenePositions();
-
-        Debug.Log(ToString());
     }
 
     // Update is called once per frame
@@ -51,6 +45,8 @@ public class Scene : MonoBehaviour
     {
         if (udpReceiver.GetComponent<UDPReceive>().gameState == GameStates.GamePhase)
             treatOrder();
+		if (udp2CppReceiver.GetComponent<UDP2CppReceive>().gameState == GameStates.GamePhase)
+			treatCalibrationData();
     }
 
     void treatOrder()
@@ -66,87 +62,16 @@ public class Scene : MonoBehaviour
             }
     }
 
-    void setScenePositions()
-    {
-        rCamera.transform.position = leftCameraPos;
-        lCamera.transform.position = rightCameraPos;
-        board.transform.position = markerBoardPos;
-        if (markerPlayerPos.Count > 0)
-            player1.transform.position = markerPlayerPos[0];
-        if (markerPlayerPos.Count > 1)
-            player2.transform.position = markerPlayerPos[1];
-        if (markerPlayerPos.Count > 2)
-            player3.transform.position = markerPlayerPos[2];
-    }
+	void treatCalibrationData()
+	{
+		calibrationOrder = udp2CppReceiver.GetComponent<UDP2CppReceive>().UDPGetPacket();
+		string[] calibrationOrders = calibrationOrder.Split('/');
 
-    bool readCalibrationData(string path)
-    {
-        try
-        {
-            string line;
-            // Create streamreader on our file
-            StreamReader reader = new StreamReader(path);
-            //Garbage collector is not handle basicly for StreamReader since it implements IDisposable interface
-            //using enables to collect all the garbage resulting of the stream reader
-            using (reader)
-            {
-                do
-                {
-                    line = reader.ReadLine();//Read current line
-
-                    if (line != null)//If line not null
-                    {
-                        string[] entries = line.Split(',');//Get all entries separating line by ','
-                        if (entries.Length > 3)//If more than 4 entry : name of the component + 3 coordinates
-                        {
-                            //Parse string to float
-                            float x = float.Parse(entries[1],
-                                System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
-                            float y = float.Parse(entries[2],
-                                System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
-                            float z = float.Parse(entries[3],
-                                System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
-
-                            //Save date according to the first entry
-                            if (entries[0] == "leftCamera")
-                                leftCameraPos = new Vector3(x, y, z);
-                            if (entries[0] == "rightCamera")
-                                rightCameraPos = new Vector3(x, y, z);
-                            if (entries[0] == "markerBoard")
-                                markerBoardPos = new Vector3(x, y, z);
-                            if (entries[0] == "markerPlayer")
-                                markerPlayerPos.Add(new Vector3(x, y, z));
-                        }
-                    }
-                }
-                while (line != null);//While not at the end of the text file
-                reader.Close();
-            }
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("{0}\n", e.Message);
-            return false;
-        }
-    }
-
-    public override string ToString()
-    {
-        string lCString = "leftCamera: " + leftCameraPos.x + ", " + leftCameraPos.y + ", " + leftCameraPos.z + "\n";
-        string rCString = "rightCamera: " + rightCameraPos.x + ", " + rightCameraPos.y + ", " + rightCameraPos.z + "\n";
-        string mBString = "markerBoard: " + markerBoardPos.x + ", " + markerBoardPos.y + ", " + markerBoardPos.z + "\n";
-        string mP1String = "";
-        string mP2String = "";
-        string mP3String = "";
-
-        if (markerPlayerPos.Count > 0)
-            mP1String = "player: " + markerPlayerPos[0].x + ", " + markerPlayerPos[0].y + ", " + markerPlayerPos[0].z + "\n";
-        if (markerPlayerPos.Count > 1)
-            mP2String = "player: " + markerPlayerPos[1].x + ", " + markerPlayerPos[1].y + ", " + markerPlayerPos[1].z + "\n";
-        if (markerPlayerPos.Count > 2)
-            mP3String = "player: " + markerPlayerPos[2].x + ", " + markerPlayerPos[2].y + ", " + markerPlayerPos[2].z + "\n";
-
-        return lCString + rCString + mBString + mP1String + mP2String + mP3String;
-    }
+		if (calibrationOrders.Length > 1)
+		if (currentCalibrationOrder.nbrOrder != int.Parse(calibrationOrders[0]))
+		{
+			currentCalibrationOrder = new CalibrationOrders(calibrationOrders);
+			currentCalibrationOrder.doCalibrationOrders();
+		}
+	}
 }
--- Unity_Joueur/Assets/script/VR_Handler/ShaderPass.cs
+++ Unity_Joueur/Assets/script/VR_Handler/ShaderPass.cs
@@ -11,16 +11,11 @@ public class ShaderPass : MonoBehaviour {
 	public Material barilletMaterial;
 	public RenderTexture barilletTexture;
 
-    //public GameObject signal;
-
 	// Use this for initialization
 	void Update () {
 		
 		// Perform the first pass, blending the 4 textures and creating one big dual screen texture
 		Graphics.Blit(null, blendPassTexture, blendPassMaterial);
-
-		//OldVersion : Pass to cut the undesirable zone
-		//Graphics.Blit(blendPassTexture, cutPassTexture, cutPassMaterial);
 
 		//Perform the second pass, to get barrillet output and cut the indesirable zone.
 		Graphics.Blit(blendPassTexture, barilletTexture, barilletMaterial);
@@ -29,11 +24,10 @@ public class ShaderPass : MonoBehaviour {
 	// Update is called once per frame
 	void Start () {
 		//Passing camera feedback to the first pass material
-		GetLeftCameraFeedback (blendPassMaterial);
-		//GetRightCameraFeedback (blendPassMaterial);
+		GetCameraFeedback (blendPassMaterial);
 	}
 
-	void GetLeftCameraFeedback (Material firstPassMaterial) {
+	void GetCameraFeedback (Material firstPassMaterial) {
 		if (Application.HasUserAuthorization (UserAuthorization.WebCam)) {
 			WebCamDevice[] devices = WebCamTexture.devices;
             //signal.GetComponentInChildren<Text>().text = "Nbr Webcams : " + devices.Length.ToString();
@@ -47,36 +41,15 @@ public class ShaderPass : MonoBehaviour {
 			WebCamTexture tex = new WebCamTexture (devices [0].name);
 			tex.wrapMode = TextureWrapMode.Repeat;
 
+			//WebCamTexture tex2 = new WebCamTexture (devices [1].name);
+			//tex2.wrapMode = TextureWrapMode.Repeat;
+
 			firstPassMaterial.SetTexture ("_MainTexLeft", tex);
             firstPassMaterial.SetTexture("_MainTexRight", tex);
 
             tex.Play ();
+			//tex2.Play ();
 		} else
 			Debug.LogError ("No permission left camera");
-	}
-
-	void GetRightCameraFeedback (Material firstPassMaterial) {
-
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
-        {//Check for camera permission
-            WebCamDevice[] devices = WebCamTexture.devices;
-
-            // for debugging purposes, prints available devices to the console
-            for (int i = 0; i < devices.Length; i++)
-            {
-                print("Webcam available: " + devices[i].name);
-            }
-
-            // assuming the first available WebCam is desired
-            WebCamTexture tex = new WebCamTexture(devices[1].name);
-            tex.wrapMode = TextureWrapMode.Repeat;
-
-            firstPassMaterial.SetTexture("_MainTexRight", tex);
-
-            tex.Play();
-        }
-        else
-            Debug.LogError("No permission right Camera");
-
 	}
 }
